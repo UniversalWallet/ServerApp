fefdddb485b7879f4ec23cfde92ba7867cbcd2f2
Vadim Koleoshkin <v.koleoshkin@gmail.com>
2016-09-18 12:44:23 +0300

    Fix + DB

---
+++ database/migrations/2016_09_18_123704_create_tickets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type');
+            $table->morphs('bidder');
+
+            $table->float('price', 10, 2)->unsigned()->default(0);
+
+            $table->timestamps();
+
+            $table->foreign('lot_id')
+                ->references('id')->on('lots')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
